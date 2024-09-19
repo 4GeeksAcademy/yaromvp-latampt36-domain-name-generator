6852d4d4d7e806f2853f32d2b2af8bbc968c1f09
yaromvp <yaromvp@gmail.com>
2024-09-19 20:14:21 +0000

    app.js modified to domain name generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "your", "my"];
+let adj = ["great", "big", "small", "winner"];
+let noun = ["jogger", "racoon", "apple", "shop"];
+let ext = [".com", ".org", ".net", ".pe"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function domainNameGenerator() {
+  let newdomain = "";
+  let index = 0;
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          newdomain = pronoun[l] + adj[k] + noun[j] + ext[i];
+          index += 1;
+          console.log(index + ". " + newdomain);
+        }
+      }
+    }
+  }
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+domainNameGenerator();
