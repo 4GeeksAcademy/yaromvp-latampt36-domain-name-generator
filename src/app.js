d9af96aedfadc1ed842a93e8c1af568d10de66b5
yaromvp <yaromvp@gmail.com>
2024-09-24 01:57:28 +0000

    camelCase y cambio de nombre a una variable

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ let noun = ["jogger", "racoon", "apple", "shop"];
 let ext = [".com", ".org", ".net", ".pe"];
 
 function domainNameGenerator() {
-  let newdomain = "";
-  let index = 0;
+  let newDomain = "";
+  let numerador = 0;
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < ext.length; l++) {
-          newdomain = pronoun[l] + adj[k] + noun[j] + ext[i];
-          index += 1;
-          console.log(index + ". " + newdomain);
+          newDomain = pronoun[l] + adj[k] + noun[j] + ext[i];
+          numerador += 1;
+          console.log(numerador + ". " + newDomain);
         }
       }
     }
